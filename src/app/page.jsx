8f26a3bf7842fe221e5d1c5c41d2e50b68d3e130
MNKolombage <mkolombage@gmail.com>
2025-10-17 23:52:22 +0530

    feat: Home page structure

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,19 +1,5 @@
-import Hero from '../components/sections/Hero'
-import OurRestaurant from '../components/sections/Our_Restaurant'
-import PlanYourTrip from '../components/sections/Plan_Your_Trip'
-import Gallery from '../components/sections/Gallery'
-import ContactSection from '../components/sections/Contact'
-import BookYourStay from '../components/sections/Book_Your_Stay'
+import HomePage from '../pages/home'
 
 export default function Page() {
-	return (
-		<>
-			<Hero />
-			<OurRestaurant />
-			<PlanYourTrip />
-			<Gallery />
-			<ContactSection />
-			<BookYourStay />
-		</>
-	)
+  return <HomePage />
 }
---
+++ src/pages/home.jsx
@@ -0,0 +1,19 @@
+import Hero from '../components/sections/Hero'
+import OurRestaurant from '../components/sections/Our_Restaurant'
+import PlanYourTrip from '../components/sections/Plan_Your_Trip'
+import Gallery from '../components/sections/Gallery'
+import ContactSection from '../components/sections/Contact'
+import BookYourStay from '../components/sections/Book_Your_Stay'
+
+export default function Home() {
+    return (
+        <>
+            <Hero />
+            <OurRestaurant />
+            <PlanYourTrip />
+            <Gallery />
+            <ContactSection />
+            <BookYourStay />
+        </>
+    )
+}
