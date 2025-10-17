65a74bbde6cd71be6440d65f66b379d690449974
MNKolombage <mkolombage@gmail.com>
2025-10-17 22:28:44 +0530

    fix: remove duplicate Header render from _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,9 @@
 import '../styles/globals.css'
 import MainLayout from '../components/layout/MainLayout'
-import Header from '../components/Header'
 
 export default function App({ Component, pageProps }) {
   return (
     <MainLayout>
-      <Header />
       <Component {...pageProps} />
     </MainLayout>
   )
